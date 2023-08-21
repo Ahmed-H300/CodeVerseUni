a0387957868fdd769e66570a052f39020c9eca17
Ahmed Hany Farouk <67925988+Ahmed-H300@users.noreply.github.com>
2023-08-21 11:45:21 +0300

    [ADD] initial commit and SQL scripts

---
+++ CodeVerseUni/src/main/java/com/codeverse/CodeVerseUni/CodeVerseUniApplication.java
@@ -0,0 +1,13 @@
+package com.codeverse.CodeVerseUni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeVerseUniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeVerseUniApplication.class, args);
+	}
+
+}
---
+++ MySQLScripts/Script1.sql
@@ -0,0 +1,53 @@
+DROP SCHEMA IF EXISTS `codeverseuni`;
+
+CREATE SCHEMA `codeverseuni`;
+
+USE `codeverseuni`;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+CREATE TABLE `instructor_detail`(
+	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `salary` DECIMAL(10, 2)
+);
+
+CREATE TABLE `instructor`(
+	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `first_name` varchar(45),
+	`last_name` varchar(45),
+	`email` varchar(45),
+     `instructor_detail_id` int ,
+     CONSTRAINT `instructor_detail_fk` FOREIGN KEY(`instructor_detail_id`) REFERENCES `instructor_detail` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `course`(
+	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `title` varchar(128),
+	`instructor_id` int ,
+    UNIQUE KEY `title_unique` (`title`),
+	CONSTRAINT `instructor_fk` FOREIGN KEY(`instructor_id`) REFERENCES `instructor` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+CREATE TABLE `review`(
+	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `comment` varchar(256),
+	`course_id` int ,
+	CONSTRAINT `course_fk` FOREIGN KEY(`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+CREATE TABLE `student`(
+	`id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `first_name` varchar(45),
+	`last_name` varchar(45),
+	`email` varchar(45)
+);
+
+CREATE TABLE `course_student`(
+	`course_id` int NOT NULL,
+    `student_id` int NOT NULL,
+    CONSTRAINT `course_student_pk` PRIMARY KEY(`course_id`, `student_id`),
+	CONSTRAINT `course_student_course_fk` FOREIGN KEY(`course_id`) REFERENCES `course` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT `course_student_student_fk` FOREIGN KEY(`student_id`) REFERENCES `student` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+SET FOREIGN_KEY_CHECKS = 1;
